248f8b1d19c839cd9207cde2717744ea0c228521
Vishal Prakapati <vishal.kumar190601@gmail.com>
2023-03-08 20:23:40 +0530

    Routes Set

--- base/views.py
+++ base/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
+rooms = [
+        {'id':"1",'name':"Lets Learn Python"},
+        {'id':"2",'name':"Lets Learn JS"},
+        {'id':"3",'name':"Lets Learn C++"},
+    ]
+
 def index(req):
     return render(req, 'base/home.html')
 
@@ -13,3 +20,12 @@ def rooms(req):
     ]
 
     return render(req, 'base/rooms.html',{"data":rooms})
+
+
+def room(req, id):
+    context = {"data":{}}
+    for data in room:
+        if data['id'] == id:
+            context['data'] = data
+            break
+    return render(req, 'base.single-room.html', context)
